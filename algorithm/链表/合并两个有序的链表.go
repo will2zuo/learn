502812f367cd9e3f85d66025bb4fe11b0fa68050
willzuo <17683115201@163.com>
2025-03-03 23:35:32 +0800

    listNode

--- algorithm/链表/合并两个有序的链表.go
+++ algorithm/链表/合并两个有序的链表.go
@@ -3,3 +3,31 @@ package main
 func main() {
 
 }
+func mergeTwoLists(h1 *ListNode, h2 *ListNode) *ListNode {
+	if h1 == nil {
+		return h2
+	}
+	if h2 == nil {
+		return h1
+	}
+	pre := &ListNode{Value: -1}
+	head := pre
+	for h1 != nil && h2 != nil {
+		if h1.Value < h2.Value {
+			pre.Next = h1
+			h1 = h1.Next
+			pre = pre.Next
+		} else {
+			pre.Next = h2
+			h2 = h2.Next
+			pre = pre.Next
+		}
+	}
+	if h1 == nil {
+		pre.Next = h2
+	}
+	if h2 == nil {
+		pre.Next = h1
+	}
+	return head.Next
+}
